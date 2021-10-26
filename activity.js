6a3bbbd835e3461b55815eec472062c0a007d2b1
Edward Paler <paler.edward@gmail.com>
2021-10-26 08:26:33 +0800

    Add activity code

---
+++ activity.js
@@ -0,0 +1,32 @@
+// 2. Find users with letter s in their first name or d in their last name.
+// - Use the $or operator.
+// - Show only the firstName and lastName fields and hide the _id field.
+
+db.users.find( 
+	{$or:[
+		{ "firstName": {$regex: 'n'}} , 
+		{ "lastName": {$regex: 'd'}}
+	]},
+	{ "_id": 0, "firstName": 1, "lastName": 1 }
+);
+
+
+// 3. Find users who are from the HR department and their age is greater then or equal to 70.
+// - Use the $and operator
+
+db.users.find( 
+	{$and:[
+		{ "department": "HR"} , 
+		{ "age": {$gte: 70}}
+	]}
+);
+
+// 4. Find users with the letter e in their first name and has an age of less than or equal to 30.
+// - Use the $and, $regex and $lte operators
+
+db.users.find( 
+	{$and:[
+		{ "firstName": {$regex: 'e'}} , 
+		{ "age": {$lte: 30}}
+	]}
+);
